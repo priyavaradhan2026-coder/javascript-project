8f8682fc3b844095ab34f469a480213c54ab4192
priyavaradhan2026-coder <priyavaradhan2026@gmail.com>
2026-05-19 12:44:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+var popupoverlay   = document.querySelector(".popup-overlay");
+var popupbox       = document.querySelector(".popup-box");
+var addpopupbutton = document.getElementById("add-popup-button");
+
+addpopupbutton.addEventListener("click", function () {
+    popupoverlay.style.display = "block";
+    popupbox.style.display     = "block";
+})
+
+var cancelbutton = document.getElementById("cancel-popup-btn");
+cancelbutton.addEventListener("click", function (event) {
+    event.preventDefault();
+    popupoverlay.style.display = "none";
+    popupbox.style.display     = "none";
+})
+
+var container            = document.querySelector(".container");
+var addbook              = document.getElementById("add-book");
+var booktitleinput       = document.getElementById("book-title-input");
+var bookauthorinput      = document.getElementById("book-author-input");
+var bookdescriptioninput = document.getElementById("book-description-input");
+
+addbook.addEventListener("click", function () {
+    if (booktitleinput.value === "") {
+        alert("Please enter a book title!");
+        return;
+    }
+
+    var div = document.createElement("div");
+    div.className = "book-container";
+    div.innerHTML = `<h2>${booktitleinput.value}</h2>
+                     <h5>${bookauthorinput.value}</h5>
+                     <p>${bookdescriptioninput.value}</p>
+                     <button type="button" onclick="deletebook(event)">Delete</button>`;
+
+    container.append(div);
+
+    popupoverlay.style.display = "none";
+    popupbox.style.display     = "none";
+
+    booktitleinput.value       = "";
+    bookauthorinput.value      = "";
+    bookdescriptioninput.value = "";
+})
+
+function deletebook(event) {
+    event.target.parentElement.remove();
+}
